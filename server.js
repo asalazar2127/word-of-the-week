4508c59ee151267e9f756cc99b409752cd68c176
Arnaldo Salazar <Salazar@Arnaldos-MacBook-Air.local>
2015-05-11 22:06:13 -0700

    added the the html lines of code

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 var http = require('http');
 
-var butthead = function(req,res) {
+var butthead = function (req,res) {
 	console.log('server hit')
-}
+	res.setHeader('Content-Type', 'text/html')
+	res.end('<h2> WORD OF THE WEEK </h2> <p> + word + </p>')
+};
+
+var word = ('hello')
+
+
 var server = http.createServer(butthead);
 server.listen(3000);
 console.log('server up and running on 3000 . . .');
