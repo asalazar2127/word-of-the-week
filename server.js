e94ad6609bf90a905ab98b66650a2c3cc7346b21
Arnaldo Salazar <Salazar@Arnaldos-MacBook-Air.local>
2015-05-11 23:28:51 -0700

    i managed to generate a random integer, i need to figure out how to link the random number with the corresdonding word in the array.

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 var http = require('http');
 
 var butthead = function (req,res) {
-	console.log('server hit')
-	res.setHeader('Content-Type', 'text/html')
-	res.end('<h2> WORD OF THE WEEK </h2> <p> + word + </p>')
+	console.log('server hit');
+
+var word = random();
+var words = ['hello', 'hola', 'hi', 'bonjour', 'chao', 'hey'];
+var index = [1,6]
+function random(index) {;
+	return Math.random() * (6-1)+1;
+};	
+        res.setHeader('Content-Type', 'text/html');
+        res.end('<h2> WORD OF THE WEEK </h2> <p>' + word + '</p>');
 };
-
-var word = ('hello')
-
-
 var server = http.createServer(butthead);
 server.listen(3000);
 console.log('server up and running on 3000 . . .');
